9c504eff2a41055a6817a588ce7684bf947ab240
Adam B <adam@prnty.com>
2018-06-30 17:20:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Hardlink Duplicate Photos.
+
+### Problem: 
+You have a folder that has a ton of duplicate photos that are scattered all of the place!
+
+### Solution:
+I created a python script that will delete all your duplicate photos but leave the filenames and directories in place pointing to one copy of the photo, which is all you need.
+
+### How does it work?
+1. Find all images that are the same size.
+
+2. Create a checksum of those images to tell which are duplicates regardless of the filename.
+
+3. Delete all duplicates images and hardlink the duplicate files to the one original file.
+
+### FAQ
+
+1. What the hell is a hardlink? **A hard link is merely an additional name for an existing file on your computer.**
+
+2. What if I edit a photo and reupload? **The newly uploaded photo would be unique and not a duplicate.**
+
+3. Can I remove the "original file" and not break the hardlink files? **Yes, saying the original file is just an easier way to explain. It actually links to an inode on the disk where the data is stored for the photo.**
+
+4. Can I run this multiple times? **Yes, Script will find all duplicates and hardlink to original file every time it runs.**
+
+5. Is there any limits or things you would do differently with update? **I would use inotify utility to run a script on any new file or modifcation. That way we would have no duplicates in real time.**
---
+++ hardlinker.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import os, hashlib
+
+### DIRECTORY OF IMAGES
+dir = "./network_share"
+
+### IMAGE EXTENTIONS
+image_exts = ("jpg", "jpeg", "png", "tiff", "gif")
+
+### INITALIZE
+image_hashes = {}
+image_sizes = {}
+total_size = 0
+total_files = 0
+total_duplicates = 0
+
+### HUMAN READABLE SIZES
+def size_format(b):
+    if b < 1000:
+              return '%i' % b + 'B'
+    elif 1000 <= b < 1000000:
+        return '%.1f' % float(b/1000) + 'KB'
+    elif 1000000 <= b < 1000000000:
+        return '%.1f' % float(b/1000000) + 'MB'
+    elif 1000000000 <= b < 1000000000000:
+        return '%.1f' % float(b/1000000000) + 'GB'
+
+### SCAN DIRECTORY RECURSIVELY 
+### GATHER IMAGE PATH AND FILE SIZE
+for root, dir, files in os.walk(dir, topdown=False):
+   for name in files:
+       full_path = os.path.join(root, name)
+       # check for image extenions       
+       if full_path.lower().endswith(image_exts): 
+           file_size = os.path.getsize(full_path)
+           # append file to array with size as key           
+           if file_size in image_sizes: 
+               image_sizes[file_size] = image_sizes[file_size] + "|" + full_path
+           else:
+               image_sizes[file_size] = full_path
+
+### LOOP THROUGH IMAGE SIZE ARRAY
+### CREATE ARRAY WITH UNIQUE CHECKSUM KEY
+for size,file_path in image_sizes.items():
+    
+    if len(file_path.split('|')) > 1:
+        for file_path_split in file_path.split('|'):
+            # create md5 checksum for images
+            file_hash = hashlib.md5(open(file_path_split, 'rb').read()).hexdigest()
+            if file_hash in image_hashes:
+                image_hashes[file_hash] = image_hashes[file_hash] + "|" + file_path_split
+            else:
+                image_hashes[file_hash] = file_path_split
+
+## LOOP IMAGES WITH SAME CHECKSUM
+## REMOVE DUPLICATES THAT SHARE SAME INODE
+## LEAVE THE PRIMARY FILE AND HARD LINK REMOVED FILES TO PRIMARY
+for image_hash,file_path in image_hashes.items():
+    files = file_path.split('|')
+    if len(files) > 1:
+        x = 0
+        for file in files:
+            # inode of primary file
+            primary_inode = os.stat(files[0]).st_ino
+            # inode of duplicate file
+            current_inode = os.stat(files[x]).st_ino
+            # keep primary and delete files with duplicate inodes 
+            if x != 0 and primary_inode != current_inode: 
+                    if os.path.isfile(file) and os.path.isfile(files[0]):
+                        os.remove(file)
+                        os.link(files[0], file)
+                        total_duplicates += 1
+                        total_size += os.path.getsize(file)
+                        # print each duplicate file removed and where it was hardlinked 
+                        print("removed ({}) -> hardlink ({})".format(file,files[0]))                    
+            x += 1
+
+### PRINT RESULTS
+if total_size == 0:
+    print("No duplicate images found.")
+else:
+    total_size = size_format(total_size)
+    total_files = len(image_sizes)
+    print("\nImages found: {}\nDuplicates removed: {}\nSpace removed: {}".format(total_files, total_duplicates, total_size))
